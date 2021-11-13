04f8f19c0bcd21bd30337e712cdc0601ac686386
IldusGaf <cooltechni@gmail.com>
2021-11-13 12:43:45 +0300

    Внес поправки для корректной работы контекста в App, и чуть оптимизировал темную тему в header

--- homework/015_lifecycle/create-react-app/src/App.tsx
+++ homework/015_lifecycle/create-react-app/src/App.tsx
@@ -2,23 +2,30 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/Header/Header';
 import ComponentWithHelper from './wrappers/ComponentWithHelper';
-import { ThemeContextProvider } from './contexts/ThemeContext';
+import { ThemeContextConsumer, ThemeContextProvider } from './contexts/ThemeContext';
 import Users from './forms/Users/Users';
 
 class App extends React.Component {
   render() {
     return (
+
       <ThemeContextProvider>
-        <div className={`App ${this.context.darkTheme && ' App_dark'}`}>
-          <Header />
-          <div className="body">
-            <div className="content">
-              <ComponentWithHelper comment="Это форма пользователей">
-                <Users />
-              </ComponentWithHelper>
-            </div>
-          </div>
-        </div>
+        <ThemeContextConsumer>
+          {
+            (context) => (
+              <div className={`App ${context.darkTheme && ' App_dark'}`}>
+                <Header />
+                <div className="body">
+                  <div className="content">
+                    <ComponentWithHelper comment="Это форма пользователей">
+                      <Users />
+                    </ComponentWithHelper>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        </ThemeContextConsumer>
       </ThemeContextProvider>
     );
   }
--- homework/015_lifecycle/create-react-app/src/components/Header/Header.tsx
+++ homework/015_lifecycle/create-react-app/src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ export class Header extends React.Component {
         {
                 (context: Partial<ThemeContextState>) => (
                   <div className={`header ${context.darkTheme && 'header_dark'}`}>
-                    <h1 className={`header__text ${context.darkTheme && 'header__text_dark'}`}>
+                    <h1 className="header__text">
                       Пользователи
                     </h1>
                     <ThemeCheckbox />
