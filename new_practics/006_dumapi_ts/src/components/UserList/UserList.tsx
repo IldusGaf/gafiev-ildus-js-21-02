6132cf9c0c5a43730999c42b5dbf9cd5934c88b0
IldusGaf <cooltechni@gmail.com>
2022-04-08 16:07:21 +0300

    Добавил Skeleton с Antd. И  Сделал пустышку карт. User для красивой анимации при загрузке

--- new_practics/006_dumapi_ts/src/components/UserList/UserList.tsx
+++ new_practics/006_dumapi_ts/src/components/UserList/UserList.tsx
@@ -4,16 +4,17 @@ import User from '../User/User';
 import { getUserList } from '../../api/dummyAPI';
 import { UserListResponse, UserType } from '../../types/dummyAPIResponses';
 import ComponentWithHelper from '../../wrappers/ComponentWithHelper/ComponentWithHelper';
-import { getPageCount, getPagesArray } from '../../utils/pages';
+import { getPageCount, getArray } from '../../utils/pages';
 import Pagination from '../Pagination/Pagination';
-import Loader from '../Loader/Loader';
+import UserTemplate from '../UserTemplate/UserTemplate';
 
 const UserList = () => {
   const [loading, setLoading] = useState(true);
   const [userList, setUserList] = useState([] as Array<UserType>);
   const [page, setPage] = useState(0);
-  const [limit, setLimit] = useState(6);
+  const [limit] = useState(8);
   const [totalCount, setTotalCount] = useState(0);
+  const limitArr = getArray(limit);
 
   useEffect(() => {
     setLoading(true);
@@ -27,20 +28,22 @@ const UserList = () => {
       () => setLoading(false));
   }, [page]);
 
-  const pageArr: Array<number> = getPagesArray(getPageCount(totalCount, limit));
+  const pageArr: Array<number> = getArray(getPageCount(totalCount, limit));
   return (
     <div className="user_page">
       <div className="user-list">
-        {loading ? <Loader /> : userList.map((elem, index) => (
-          <ComponentWithHelper idComment={elem.id} key={index}>
-            <User
-              title={elem.title}
-              picture={elem.picture}
-              firstName={elem.firstName}
-              lastName={elem.lastName}
-            />
-          </ComponentWithHelper>
-        ))}
+        {loading
+          ? limitArr.map((elem) => <UserTemplate key={elem} />)
+          : userList.map((elem, index) => (
+            <ComponentWithHelper idComment={elem.id} key={index}>
+              <User
+                title={elem.title}
+                picture={elem.picture}
+                firstName={elem.firstName}
+                lastName={elem.lastName}
+              />
+            </ComponentWithHelper>
+          ))}
       </div>
       <Pagination totalPages={pageArr} currentPage={page} callback={setPage} />
     </div>
--- new_practics/006_dumapi_ts/src/utils/pages.ts
+++ new_practics/006_dumapi_ts/src/utils/pages.ts
@@ -1,6 +1,6 @@
 /*eslint-disable*/
 export const getPageCount = (totalCount: number, limit: number): number => Math.ceil(totalCount / limit);
-export const getPagesArray = (totalPages: number) => {
+export const getArray = (totalPages: number) => {
   const result = [];
   for (let i = 0; i < totalPages; i++) {
     result.push(i);
